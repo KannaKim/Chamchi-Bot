81096beac970362f399664f987db8c588a80f1ff
Kanna Kim <kimkanna18@gmail.com>
2022-11-29 23:23:59 -0600

    added sell command (it was ommitted before)

---
+++ commands/sell.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require('discord.js');
+const balance = require('../factory/balance')
+const discord_util = require("../utill/discord")
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('판매')
+		.setDescription('특정 자산을 참치로 교환합니다.')
+        .addStringOption(option =>
+            option.setName('종류')
+                .setDescription('자산의 종류')
+                .addChoices({
+					name: "카지노칩",
+					value: "카지노칩"
+				})
+                .setRequired(true))
+            .addIntegerOption(option=> 
+                option.setName("양")
+                .setDescription("판매할 자산의 양")
+                .setMinValue(1)
+                .setRequired(true)),
+	async execute(interaction) {
+        let msg = ""
+        if(interaction.options.data.length == 2){   //userid: interaction.user.id
+            msg = await balance.sell(interaction.user.id, interaction.options.data[0].value, interaction.options.data[1].value)
+        }
+        await interaction.reply(msg)
+	},
+};``

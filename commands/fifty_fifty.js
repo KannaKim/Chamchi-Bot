43a7a211f5c7eae7006a8c82c2b71c6711f69490
Kanna Kim <kimkanna18@gmail.com>
2023-01-31 20:39:42 -0600

    added max value for fifty fifty

--- commands/fifty_fifty.js
+++ commands/fifty_fifty.js
@@ -4,11 +4,11 @@ const {fifty_fifty} = require("../casino_config.json")
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('홀짝')
-		.setDescription(`칩을 사용해 홀/짝에 베팅을 합니다.\n승리시 2배의 칩을 받으며 ${fifty_fifty.fee_in_percentages}%에 수수료가 부가됩니다. 최소 베팅량은 100입니다. \n사용방법: /홀짝 <베팅양> <홀|짝>\n`,[]
+		.setDescription(`칩을 사용해 홀/짝에 베팅을 합니다.\n승리시 2배의 칩을 받으며 ${fifty_fifty.fee_in_percentages}%에 수수료가 부가됩니다. 최소 베팅량은 100 칩입니다. 최대 베팅량은 10000 칩 입니다.\n사용방법: /홀짝 <베팅양> <홀|짝>\n`,[]
         +'사용예시:\n/홀짝 100 홀')
         .addIntegerOption(option =>
 			option.setName('베팅양')
-				.setDescription('베팅할 칩의 양, 정수만 가능합니다.').setMinValue(100)
+				.setDescription('베팅할 칩의 양, 정수만 가능합니다.').setMinValue(100).setMaxValue(100000)
 				.setRequired(true))
 		.addStringOption(option =>
             option.setName('홀짝')
